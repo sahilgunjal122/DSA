9c4de452c08ae6e2d68f344e97def11da4880014
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2023-12-18 18:48:06 +0530

    New Questions Added

--- LOVE BABBAR/045_Array_LB.cpp
+++ LOVE BABBAR/045_Array_LB.cpp
@@ -58,24 +58,72 @@ class DynamicArray
 
         cout<<"Is Vector Empty "<<crr.empty()<<endl;  // 0 for False
 
-
-
-
-
         
     }   
 
 };
 
+//Unique Number In an Array
 int uniquenumber(vector<int> arr)
 {
     int ans=0;
 
     for (int i = 0; i < arr.size(); i++)
     {
-        ans=ans^arr[i];
+        ans=ans^arr[i];    //XOR Return False when Elements are same Otherwise True.
     }
     return ans;
+}
+
+//UNION OF ARRAY
+
+void UnionArray(vector<int> arr,vector<int> brr)
+{
+     vector<int> ans;
+    //Elements of Array 1
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans.push_back(arr[i]);
+    }
+    //Elements of Array 2
+    for (int i = 0; i < brr.size(); i++)
+    {
+        ans.push_back(brr[i]);
+    }
+
+    //Print Ans Array
+    cout<<"Union of Numbers:"<<endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+}
+
+//INTERSECTION OF ARRAY
+
+int InterSectionArray(vector<int> arr,vector<int> brr)
+{
+    vector<int> ans;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int element=arr[i];
+        for (int j = 0; j < brr.size(); j++)
+        {
+            if (element==brr[j])
+            {
+                //mark
+                brr[j]=-1;  //just for positive Numbers otherwise use INT_MIN
+                ans.push_back(element);
+            }
+            
+        }
+        
+    }
+    cout<<"InterSection Array:"<<endl;
+    for(auto value:ans)
+    {
+        cout<<value<<" "; 
+    }
 }
 
 
@@ -83,24 +131,46 @@ int uniquenumber(vector<int> arr)
 int main()
 {
     // DynamicArray d1;
-    // d1.function();   
+    // d1.function();  
 
-   int n;
-   cout<<"Enter the Size of the Array:"<<endl;
-   cin>>n;
-   vector<int> vec(n);
+    //UNIQUE NUMBER
+//    int n;
+//    cout<<"Enter the Size of the Array:"<<endl;
+//    cin>>n;
+//    vector<int> vec(n);
 
-    cout<<"Enter Elements:"<<endl;
-    for (int i = 0; i < vec.size(); i++)
-    {
-        cin>>vec[i];
-    }
+//     cout<<"Enter Elements:"<<endl;
+//     for (int i = 0; i < vec.size(); i++)
+//     {
+//         cin>>vec[i];
+//     }
+//    int Result=uniquenumber(vec);
+//    cout<<"The Unique Number is "<<Result<<endl;
+
+    //UNION OF TWO ARRAY
+
+    // vector<int> arr1{1,2,3,4,5};
+    // vector<int> brr1{6,7};
+
+    // UnionArray(arr1,brr1);
+
+
+    //INTERSECTION OF ARRAY
+
+    // vector<int> arr2{1,2,4,4,5};
+    // vector<int> brr2{4,6,8,9};
+
+    // InterSectionArray(arr2,brr2);
     
 
-   int Result=uniquenumber(vec);
-   cout<<"The Unique Number is "<<Result<<endl;
+    
+
+
 
     
+
+
+
     
     
     
