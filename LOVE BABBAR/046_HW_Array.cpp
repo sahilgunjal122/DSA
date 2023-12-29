854642cf274c56c4d8df517da86e45d4f937cc44
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2023-12-29 10:01:11 +0530

    HW Done

---
+++ LOVE BABBAR/046_HW_Array.cpp
@@ -0,0 +1,286 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1.Color Sort(LeetCode)
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        // STL
+        // sort(nums.begin(),nums.end());
+
+        // count Method
+        //  int zeros,ones,twos;
+        //  zeros=ones=twos=0;
+        //  for(int i=0;i<nums.size();i++)
+        //  {
+        //      if(nums[i]==0)
+        //      {
+        //          zeros++;
+        //      }
+        //      else if(nums[i]==1)
+        //      {
+        //          ones++;
+        //      }
+        //      else{
+        //          twos++;
+        //      }
+        //  }
+
+        // int i=0;
+        // while(zeros--)
+        // {
+        //     nums[i]=0;
+        //     i++;
+        // }
+        // while(ones--)
+        // {
+        //     nums[i]=1;
+        //     i++;
+        // }
+        // while(twos--)
+        // {
+        //     nums[i]=2;
+        //     i++;
+        // }
+
+        // 3 Pointer Approach "in-palce"
+
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+// 2.Move all -ve Numbers to the Left
+
+void leftofArrray(int *nums, int n)
+{
+    // 2 Pointer approach(Dutch National Flag)
+    int low = 0, high = n - 1;
+
+    while (low <= high)
+    {
+        if (nums[low] < 0)
+        {
+            low++;
+        }
+        else if (nums[high] > 0)
+        {
+            high--;
+        }
+        else
+        {
+            swap(nums[low], nums[high]);
+            low++;
+            high--;
+        }
+    }
+}
+
+// 3.Find Duplicate elemeny in the array
+
+int Duplicate(vector<int> arr, int n)
+{
+    // Normal Method
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      if (arr[i] == arr[i + 1])
+    //      {
+    //          return arr[i];
+    //      }
+    //  }
+
+    // Negative Marking
+    int ans = -1;
+    for (int i = 0; i < n; i++)
+    {
+        int index = abs(arr[i]);
+        if (arr[index] < 0)
+        {
+            ans = index;
+        }
+        arr[index] *= -1;
+    }
+    return ans;
+}
+
+// 4.Missing Element With Duplicates(1-Based Index)
+
+int MissingElement(vector<int> arr, int n)
+{
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int index = abs(arr[i]);
+        if (arr[index - 1] > 0)
+        {
+            arr[index - 1] *= -1;
+        }
+    }
+
+    for (auto val : arr)
+    {
+        cout << val << " ";
+    }
+
+    // all positive Numbers are Missing
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > 0)
+        {
+            cout << "\nMissing Number is : " << i + 1 << endl;
+        }
+    }
+}
+
+// 5.First repeating Element
+
+int repeating(vector<int> arr, int n)
+{
+    // Time Complexity: O(n^2) it should be O(n)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     bool isRepeated = false;
+
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if (arr[i] == arr[j])
+    //         {
+    //             isRepeated = true;
+    //             return i + 1;
+    //         }
+    //     }
+    // }
+    // return -1;
+
+    // optimized Solution
+
+    // unordered_map<int, int> hash;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     hash[arr[i]]++;
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (hash[arr[i]] > 1)
+    //     {
+    //         return i + 1;
+    //     }
+    // }
+    // return -1;
+
+    // without using unordered_map
+
+    int hash[6];
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (hash[arr[i]] > 1)
+        {
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
+// 6.Comman Element in 3 Sorted Array
+
+vector<int> commonelement(vector<int> A, vector<int> B, vector<int> C, int n1, int n2, int n3)
+{
+    vector<int> ans;
+    set<int> st;
+    int i, j, k;
+    i = j = k = 0;
+    while (i < n1 && j < n2 && k < n3)
+    {
+        if (A[i] == B[j] && B[j] == C[k])
+        {
+            st.insert(A[i]);
+            i++;
+            j++;
+            k++;
+        }
+        else if (A[i] < B[j])
+        {
+            i++;
+        }
+        else if (B[i] < C[k])
+        {
+            j++;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    for (auto val : st)
+    {
+        ans.push_back(val);
+    }
+
+    // for (auto val : ans)
+    // {
+    //     cout << val << " ";
+    // }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr{1, 3, 5, 3, 7, 5};
+    // 2.Negative Number on Left
+    //  leftofArrray(arr, 7);
+
+    // for (auto val : arr)
+    // {
+    //     cout << val << " ";
+    // }
+
+    // 3.Find Duplicate Number
+    // int result = Duplicate(arr, 8);
+    // cout << result;
+
+    // 4.Missing Element In Arrya With Duplicate
+    // MissingElement(arr, 5);
+
+    // 5.Repeating Element
+    // cout << repeating(arr, 6);
+
+    // 6.Comman Element in 3 Array
+
+    vector<int> A{1, 5, 10, 20, 40, 80};
+    vector<int> B{6, 7, 20, 80, 100};
+    vector<int> C{3, 4, 15, 20, 30, 70, 80, 120};
+
+    vector<int> ans;
+    ans = commonelement(A, B, C, 6, 5, 8);
+
+    for (auto val : ans)
+    {
+        cout << val << " ";
+    }
+    return 0;
+}
--- LOVE BABBAR/tempCodeRunnerFile.cpp
+++ LOVE BABBAR/tempCodeRunnerFile.cpp
@@ -1,4 +1,5 @@
-vector<int> arr2{1,2,2,1};
-    vector<int> brr2{2,2};
 
-    InterSectionArray(arr2,brr2);
+    // for (auto val : ans)
+    // {
+    //     cout << val << " ";
+    // }
