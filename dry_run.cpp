291a68541bcdb99152b932de5564fd01df221f88
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2024-07-18 12:03:50 +0530

    Some Changes

--- dry_run.cpp
+++ dry_run.cpp
@@ -1,91 +1,41 @@
 #include <iostream>
+#include <vector>
 #include <bits/stdc++.h>
 using namespace std;
 
-int maximum(vector<int> arr)
+class Solution
 {
-    int max = INT8_MIN;
-
-    for (int i = 0; i < arr.size(); i++)
+public:
+    int majorityElement(vector<int> &nums)
     {
-        if (arr[i] > max)
+        int count = 0;
+        int n = nums.size();
+
+        vector<int> hash{n + 1, 0};
+
+        for (int i = 0; i < n; i++)
         {
-            max = arr[i];
+            hash[nums[i]]++;
         }
-    }
-    return max;
-}
 
-int minimum(vector<int> arr)
-{
-    int min = INT8_MAX;
-
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (arr[i] < min)
+        int max = INT_MIN;
+        for (int i = 0; i < n; i++)
         {
-            min = arr[i];
+            if (hash[i] > max)
+            {
+                max = hash[i];
+            }
         }
+        return hash[max];
     }
-    return min;
-}
-
-void first_last(vector<int> arr)
-{
-    int start = 0;
-    int end = arr.size() - 1;
-
-    while (start <= end)
-    {
-        if (start > end)
-            break;
-
-        cout << arr[start] << " ";
-        cout << arr[end] << " ";
-        start++;
-        end--;
-    }
-}
-
-void reverseeeeee(vector<int> arr)
-{
-    int start = 0;
-    int end = arr.size() - 1;
-
-    while (start <= end)
-    {
-        if (arr[start] < arr[end])
-        {
-            int temp;
-            temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-        }
-        start++;
-        end--;
-    }
-
-    for (auto it : arr)
-    {
-        cout << it << " ";
-    }
-}
+};
 
 int main()
 {
-    vector<int> arr{10, 20, 40, 60, 3, 367};
-    int ans1, ans2;
-    ans1 = maximum(arr);
-    ans2 = minimum(arr);
-    cout << "The Maximum Number is:" << ans1 << endl;
-    cout << "The Minimum Number is:" << ans2 << endl;
-
-    cout << "This is First Last:" << endl;
-    first_last(arr);
-    cout << endl;
-    cout << "This is Reverse" << endl;
-    reverseeeeee(arr);
-    // printttttt(arr);
-
+    Solution s1;
+    vector<int> nums{3, 2, 3};
+    int ans;
+    ans = s1.majorityElement(nums);
+    cout << "THis is the ans:" << ans;
     return 0;
 }
