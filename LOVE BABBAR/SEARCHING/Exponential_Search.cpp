1d8dd2f3ad7e454293838cd11d91c781be0963d6
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2024-09-11 21:30:57 +0530

    Exponentail Seacrh Added

---
+++ LOVE BABBAR/SEARCHING/Exponential_Search.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[] = {3, 4, 5, 6, 11, 13, 14, 15, 56, 70};
+    int n = sizeof(a) / sizeof(int);
+    cout << "The size of Array:" << n << endl;
+    return 0;
+}
