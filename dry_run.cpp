e968ba8d699ca6fc4cb69cdc068bbd957ce181d8
SahilGunjal <gunjal.zswyv21@sinhgad.edu>
2024-10-01 10:57:03 +0530

    Unbounded Search

--- dry_run.cpp
+++ dry_run.cpp
@@ -1,39 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> arr, int n)
+class Solution
 {
-    for (int i = 1; i < n; i++)
+public:
+    bool isPalindrome(string s)
     {
-        int temp = arr[i];
-        int j = i - 1;
-        for (; j >= 0; j--)
+        int i = 0, j = s.length() - 1;
+
+        while (i <= j)
         {
-            if (arr[j] > temp)
+            if (s[i] == s[j])
             {
-                arr[j + 1] = arr[j];
-            }
-            else
-            {
-                break;
+                return true;
             }
         }
-        arr[j + 1] = temp;
+        return false;
     }
-
-    for (auto it : arr)
-    {
-        cout << it << " ";
-    }
-}
+};
 
 int main()
 {
-
-    vector<int> arr{10, 45, 36, 58, 30};
-    int n = 5;
-
-    merge(arr, n);
+    Solution s1;
+    string s = " ";
+    bool ans = s1.isPalindrome(s);
+    if (ans == true)
+        cout << "Is A Palindrome";
+    else
+        cout << "Is not";
 
     return 0;
 }
